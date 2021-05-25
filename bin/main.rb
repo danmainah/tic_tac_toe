5014fd4d883405ce15b40674c9e47330404869ce
danmainah <danielmainawaweru@gmail.com>
2021-05-25 16:26:23 +0300

    Fix the styling errors

--- bin/main.rb
+++ bin/main.rb
@@ -51,68 +51,69 @@ puts "\n #{one} starts the game"
 
 @players = Players.new(one, two)
 
-
 # change_turn method handles position choice and switching player turns
 def change_turn(player, symbol)
-    puts "#{player}(#{symbol}) please choose a position"
-    @player_move = gets.chomp.to_i
-    if (0..8).include?(@player_move) && @gameboard.board[@player_move] == ' ' && @end == false
-      @turn += 1
-      @gameboard.board_update(@player_move, symbol)
-      win
-      draw
-    else
-      puts "Please enter a number between 0 to 8 in an untaken location\n"
+  puts "#{player}(#{symbol}) please choose a position"
+  @player_move = gets.chomp.to_i
+  if (0..8).include?(@player_move) && @gameboard.board[@player_move] == ' ' && @end == false
+    @turn += 1
+    @gameboard.board_update(@player_move, symbol)
+    win
+    draw
+  else
+    puts "Please enter a number between 0 to 8 in an untaken location\n"
+  end
+end
+
+WIN_NUM = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+].freeze
+
+@players = Players.new(one, two)
+# check for a win
+def win
+  WIN_NUM.each do |win|
+    next unless (@gameboard.board[win[0]] == @gameboard.board[win[1]] &&
+      @gameboard.board[win[1]] == @gameboard.board[win[2]]) &&
+                @gameboard.board[win[0]] != ' '
+
+    case @gameboard.board[win[0]]
+    when 'X'
+      puts "#{@players.player1} WINS"
+      @turn = 10
+      @end = true
+    when 'O'
+      puts "#{@players.player2} WINS"
+      @turn = 10
+      @end = true
     end
   end
-  
-  WIN_NUM = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [6, 4, 2]
-  ]
-  
-  @players = Players.new(one, two)
-  # check for a win
-  def win
-    WIN_NUM.each do |win|
-      next unless (@gameboard.board[win[0]] == @gameboard.board[win[1]] &&
-        @gameboard.board[win[1]] == @gameboard.board[win[2]]) &&
-                  @gameboard.board[win[0]] != ' '
-  
-      case @gameboard.board[win[0]]
-      when 'X'
-        puts "#{@players.player1} WINS"
-        @turn = 10
-        @end = true
-      when 'O'
-        puts "#{@players.player2} WINS"
-        @turn = 10
-        @end = true
-      end
+end
+
+# check for a draw
+def draw
+  puts "It's a draw" if @turn == 10 && @end == false
+end
+
+# gameplay method
+def move
+  @end = false
+  @gameboard = Board.new
+  # @players = Players.new(one, two)
+  @turn = 1
+  while @turn < 10
+    if @turn.odd?
+      change_turn(@players.player1, 'X')
+    elsif @turn.even?
+      change_turn(@players.player2, 'O')
     end
   end
-  # check for a draw
-  def draw
-    puts "It's a draw" if @turn == 10 && @end == false
-  end
-  # gameplay method
-  def move
-    @end = false
-    @gameboard = Board.new
-    # @players = Players.new(one, two)
-    @turn = 1
-    while @turn < 10
-      if @turn.odd?
-        change_turn(@players.player1, 'X')
-      elsif @turn.even?
-        change_turn(@players.player2, 'O')
-      end
-    end
-  end
-  move
+end
+move
--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,8 @@
 class Players
-    attr_accessor :player1, :player2
-  
-    def initialize(one, two)
-      @player1 = one
-      @player2 = two
-    end
+  attr_accessor :player1, :player2
+
+  def initialize(one, two)
+    @player1 = one
+    @player2 = two
   end
-  
+end
