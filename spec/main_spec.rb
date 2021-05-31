5ceda63b4a1de31e028a1052a84b79e454188c1a
danmainah <danielmainawaweru@gmail.com>
2021-05-31 11:31:58 +0300

    Add test for win and draw methods

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -6,3 +6,28 @@ context '#move' do
     expect(true).to eql(true)
   end
 end
+
+describe '#change_turn' do
+    it 'check if number is in the board' do
+      return true if (0..8).include?(@player_move)
+  
+      expect(true).to eql(true)
+    end
+  end
+
+  
+describe '#win' do
+    let(:wins) {[0, 1, 2]}
+    let(:WIN_NUM) {[[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8],[6, 4, 2]]}
+    it "it should return the win if wins is in the steps" do
+      expect(WIN_NUM).to include(wins)
+  end
+  end
+  
+  describe '#draw' do
+    it 'checks if draw has been achieved in the game' do
+      return true if @turn == 10 && @end == false
+  
+      expect(true).to eql(true)
+    end
+  end
